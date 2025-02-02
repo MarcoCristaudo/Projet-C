dfb93cb3982be968fee004af2a80bd3308e820dd
MarcoCristaudo <158455097+MarcoCristaudo@users.noreply.github.com>
2025-02-03 00:45:15 +0100

    Update Caffichage.c

--- Caffichage.c
+++ Caffichage.c
@@ -35,12 +35,13 @@ void afficheTerrainNeutre()
 //fonction qui affiche les vagues AVANT le debut du jeu
 void afficheVagues(FILE* fichierVague)
 {
+    //rewind(fichierVague);
     //remplir le tableau de points 
     char affichage[NOMBRE_LIGNES][NOMBRE_TOURS]; //7 lignes et 9 colonnes
     for (int i = 0; i < NOMBRE_LIGNES; i++)
     {
         for (int j = 0; j < NOMBRE_TOURS; j++)
-            affichage[i][j] = '.';
+            affichage[i][j] = ' ';
     }
     
     int numeroTour = 0;
@@ -54,15 +55,64 @@ void afficheVagues(FILE* fichierVague)
     fscanf(fichierVague, "%d %d %c", &numeroTour, &numeroLigne, &typeFichier);
     affichage[numeroLigne-1][numeroTour-1] = typeFichier;
     } while (fgets(chaine, 100, fichierVague) != NULL);
-    
-    //afficher le tableau final
-    for (int i = 0; i < NOMBRE_LIGNES; i++)
-    {
-        printf("%d|", i+1);
-        for (int j = 0; j < NOMBRE_TOURS; j++)
-            printf("%c\t", affichage[i][j]);
-        printf("\n");
+    //setenv("XMODIFIERS", "@im=none", 1);
+    printf("Debug : Avant InitWindow()\n");
+    InitWindow(1700,1000,"vague");
+    SetTargetFPS(30); 
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        for (int i = 1; i <=NOMBRE_LIGNES; i++) {
+            for (int j = 1; j <=NOMBRE_TOURS; j++) {
+            DrawRectangleLines(X + (j-1) * TAILLE_CASE, Y + (i-1) * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE, GRAY);
+            }
+        }
+    for (int i=0; i <NOMBRE_LIGNES; i++){
+        for (int j=0; j<NOMBRE_TOURS; j++){
+            DrawText(TextFormat("tour: %d",(j+1)),X +(j)*TAILLE_CASE+10,110,20, WHITE);
+            DrawText(TextFormat("\n\n   %c",affichage[i][j]),X +(j)*TAILLE_CASE+10, Y+(i)*TAILLE_CASE+5,20, WHITE);
+        }
     }
+        DrawText("ZOMBIES A ERADIQUER", 1200, 100, 30, RED);
+
+    DrawText("CLASSIQUE Z", 1200, 150, 25,RED);
+    DrawText("   o}  PV : 5", 1220, 180, 20, WHITE);
+    DrawText("  >-§  DEGATS : 1", 1220, 200, 20, WHITE);
+    DrawText(" _/_|  VITESSE : 1", 1220, 220, 20, WHITE);
+    
+    DrawText("MASSIF M", 1200, 280, 25,RED);
+    DrawText(" __o   PV : 9", 1220, 310, 20, WHITE);
+    DrawText("   (.) DEGATS : 1", 1220, 330, 20, WHITE);
+    DrawText(" _/_|  VITESSE : 0.5", 1220, 350, 20, WHITE);
+    
+    DrawText("DOCTEUR D", 1200, 410, 25,RED);
+    DrawText("  <+>  PV : 4", 1220, 440, 20, WHITE);
+    DrawText("  _0   DEGATS : 1", 1220, 460, 20, WHITE);
+    DrawText("  /|   VITESSE : 1", 1220, 480, 20, WHITE);
+    
+    DrawText("SPEEDY S", 1200, 540, 25,RED);
+    DrawText("   >°  PV : 3", 1220, 570, 20, WHITE);
+    DrawText("  _§_  DEGATS : 1 VITESSE : 4", 1220, 590, 20, WHITE);
+    
+    DrawText(" ?#?#?## X", 1200, 650, 25,RED);
+    DrawText(" _<?>  PV: ? 1 - 8 ?", 1220, 680, 20, WHITE);
+    DrawText("   #>  DEGATS: # 1 - 6 ?", 1220, 700, 20, WHITE);
+    DrawText("   /|  VITESSE:? 1 - 3 ?", 1220, 720, 20, WHITE);
+    
+    DrawText("KAMIKAZE K", 1200, 780, 25,RED);
+    DrawText("     o     PV : 2", 1220, 810, 20, WHITE);
+    DrawText("   <| ! |> DEGATS : 10", 1220, 830, 20, WHITE);
+    DrawText("   oooo    VITESSE : 3", 1220, 850, 20, WHITE);
+    DrawText("(Tapez ENTER pour passer à la suite)", 700, 900, 40, WHITE);
+        EndDrawing();
+        if (IsKeyPressed(KEY_ENTER)) {
+            break;
+        }
+
+    }
+    
+    UnloadFont(GetFontDefault());
+    CloseWindow();
 
 }
 
@@ -71,7 +121,7 @@ void affichejeu(Jeu *jeu) {
     jeu->score=jeu->cagnotte;//plus on à dépensé d'argent, moins notre score au début est bon
     setenv("XMODIFIERS", "@im=none", 1);// on désactive l'IME (un systeme qui nous permet d'écrire des caractères en chinois,japonais etc qui nous sont inutile ici) pour éviter certaines fuites mémoire de raylib
     InitWindow(1700, 1200, "nom_jeu"); // Initialise la fenêtre
-    SetTargetFPS(10); // Réduit la vitesse du jeu
+    SetTargetFPS(30); // Réduit la vitesse du jeu
 
     while (!WindowShouldClose() && jeu->nombre_etudiant != 0) {
         BeginDrawing();
@@ -83,7 +133,7 @@ void affichejeu(Jeu *jeu) {
             //on alterne la couleur des cases
             Color color = ((i + j) % 2 == 0) ? GREEN : DARKGREEN;
             DrawRectangle(X + j * TAILLE_CASE, Y + i * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE, color);
-            DrawRectangleLines(X + j * TAILLE_CASE, Y + i * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE, GRAY);
+            //DrawRectangleLines(X + j * TAILLE_CASE, Y + i * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE, GRAY);
             }
         }
 
@@ -101,13 +151,11 @@ void affichejeu(Jeu *jeu) {
             e = e->next;
         }
         
-        // Affichage du score et de la cagnotte avec un fond et une police plus visible
-        DrawRectangle(10, 830, 200, 60, BLACK);
-        DrawText(TextFormat("Score: %d", jeu->score), 20,830, 20, WHITE);
-        //DrawText(TextFormat("Cagnotte: %d", jeu->cagnotte), 20, SCREEN_HEIGHT - 40, 20, WHITE);
+        // on affiche le score
+        DrawRectangle(10, 840, 200, 60, BLACK);
+        DrawText(TextFormat("Score: %d", jeu->score), 20,850, 20, WHITE);
         sleep(1);
         EndDrawing();
-
         jeu->tour+=1;
         actionsTourelles(jeu);//on actionne les tourelles
         Etudiant *courant1=jeu->etudiants;
@@ -126,8 +174,15 @@ void affichejeu(Jeu *jeu) {
             }
         }
     }
+    UnloadFont(GetFontDefault());
+    
     CloseWindow();
-    gagner(jeu);
+    if(jeu->nombre_etudiant!= 0){
+        perdu(jeu);
+    }
+    else{
+        gagner(jeu);
+    }
 }
 
 
@@ -141,19 +196,48 @@ affichejeu(Jeu *jeu) affiche les Etudiants avec leurs PV tour par tour, tant qu'
 /* perdu() s'occupe de l'affichage en cas de victoire*/
 void perdu(Jeu* jeu){
     //system("clear");//si vous souhaiter voir toute les étapes mettez cette ligne en commentaire
-    printf("Vous avez perdu gros Neuille\n");
     liberer_tourelle(jeu);
     liberer_etudiant(jeu);
     free(jeu);
+    setenv("XMODIFIERS", "@im=none", 1);
+    InitWindow(800, 600,"gagner"); 
+    SetTargetFPS(30); 
+    while (!WindowShouldClose()) { 
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("VOUS AVEZ PERDU !",100,250,50,RED);
+        DrawText("(Appuyez sur ESPACE pour quitter)",150,350,20,WHITE);
+        EndDrawing();
+        if (IsKeyPressed(KEY_SPACE)) {
+            break;
+        }
+    }
+    UnloadFont(GetFontDefault());
+    CloseWindow();
 }
 /* perdu() s'occupe de l'affichage en cas de victoire*/
 void gagner(Jeu* jeu){
     ajouter_meilleurs_scores(jeu->score);
-    //system("clear");
     liberer_etudiant(jeu);
     liberer_tourelle(jeu);
+    setenv("XMODIFIERS", "@im=none", 1);
+    InitWindow(800, 600,"gagner"); 
+    SetTargetFPS(30); 
+    while (!WindowShouldClose()) { 
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("VOUS AVEZ GAGNÉ !",100,250,50,GREEN);
+        DrawText(TextFormat("Votre score est de %d", jeu->score), 150,300,30, WHITE);
+        DrawText("(Appuyez sur ESPACE pour quitter)",150,350,20,WHITE);
+        EndDrawing();
+        if (IsKeyPressed(KEY_SPACE)) {
+            break;
+        }
+    }
+    UnloadFont(GetFontDefault());
+    
+    CloseWindow();
     free(jeu);
-    printf("Vous avez gagné\n");
 }
 
 //fonction qui verifie si le fichier est conforme
@@ -211,11 +295,11 @@ void tri_par_insertion(int tab[], int taille) {
         int j=i-1;
 
         // Déplacer les éléments du tableau[0..i-1] plus grands que cle
-        while (j>= 0 && tab[j]< cle) {
-            tab[j+1] = tab[j];
+        while (j>=0 && tab[j]<cle) {
+            tab[j+1]=tab[j];
             j--;
         }
-        tab[j+1] = cle;
+        tab[j+1]=cle;
     }
 }
 
@@ -226,7 +310,7 @@ void ajouter_meilleurs_scores(int nv_score){
     FILE *fichier = fopen("meilleur_score.txt", "r");
     
     int taille=10;
-    int tab[10]={-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+    int tab[10]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
     char debut[20];
     fgets(debut,sizeof(debut),fichier);//on saute la première ligne ou il y a la cagnotte.
     int score;
@@ -278,6 +362,7 @@ void afficher_meilleurs_scores(){
         return;
     }
     int score;
+
     if(!fichier){//on crée le fichier si il n'existe pas
         printf("Il n 'y a pas de scores enregistré\n");
         fichier= fopen("meilleur_score.txt", "w");
@@ -287,10 +372,33 @@ void afficher_meilleurs_scores(){
     }
     char debut[20];
     fgets(debut,sizeof(debut),fichier);//on saute la première ligne ou il y a la cagnotte.;
+    int affichage[10]={0,0,0,0,0,0,0,0,0,0};
+    int i=0;
     while (fscanf(fichier, "%d", &score) == 1) {//on insère les scores enregistrés dans les parties précédentes dans un tableau temporaire
-        printf("%d\n",score);
+        affichage[i]=score;
+        printf("%d",affichage[i]);
+        i+=1;
     }
     fclose(fichier);
+    setenv("XMODIFIERS", "@im=none", 1);
+    InitWindow(1700, 1200, "Meilleurs Scores");
+    SetTargetFPS(30);
+    
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Meilleurs Scores", 550, 50, 50, GREEN);
+        for (int i = 0; i < 10; i++) {
+            DrawText(TextFormat(" %d :  %d ",(i+1) ,affichage[i]), 700, 150 + (i * 60), 30, WHITE);
+        }
+        DrawText("Appuyez sur ESPACE pour quitter", 550, 750, 20, GRAY);
+        EndDrawing();
+        if (IsKeyPressed(KEY_SPACE)) {
+            break;
+        }
+    }
+    UnloadFont(GetFontDefault());
+    CloseWindow();
 }
 
 
